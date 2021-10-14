86a65e21f8f6721dbfa112fd26aa6cf4e8196c16
David THIBAU <david.thibau@gmail.com>
2021-10-14 16:04:42 +0200

    Fix versions

--- library/src/test/java/hello/service/MyServiceTest.java
+++ library/src/test/java/hello/service/MyServiceTest.java
@@ -1,15 +1,13 @@
 package hello.service;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest("service.message=Hello")
 public class MyServiceTest {
 
